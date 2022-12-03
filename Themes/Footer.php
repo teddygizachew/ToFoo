b05af441a9094f7dabe42e2586ac19fe6065599a
Angle-M <98354046+Angle-M@users.noreply.github.com>
2022-12-03 00:14:26 -0500

    Landing Page
    The Header and Footer have been made, will work on setting up the rest of the landing page tomorrow

---
+++ Themes/Footer.php
@@ -0,0 +1,22 @@
+</body>
+<?php
+    require_once("Themes/Settings.php")
+?>
+<footer class="fixed-bottom" style="background-color:lightcoral">
+<div class="container text-center" style="background-color:lightcoral">
+  <div class="row">
+    <div class="col">
+      <h3>ToFoo: Food Delivery Service</h3>
+    </div>
+    <div class="col">
+            <a class="list-group-item" href="#">About us</a>
+            <a class="list-group-item" href="#">Terms of Service</a>
+            <a class="list-group-item" href="#">Help</a>
+    </div>
+    <div class="col">
+      <h3>Place holder</h3>
+    </div>
+  </div>
+</div>
+</footer>
+</html>
