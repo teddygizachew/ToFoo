626f609cad8e6be106ef3b6545e851db123b5fed
Angle-M <98354046+Angle-M@users.noreply.github.com>
2022-12-05 18:34:31 -0500

    Got the query working
    I'll meet with the professor tomorrow to see how to get information to populate and then ask about other things for the project

--- Themes/Header.php
+++ Themes/Header.php
@@ -1,7 +1,7 @@
 <?php
 //This loads the required bootstrap styles, html header, and required meta tags
 // makes the code feel cleaner
-  require_once("Themes/Settings.php");
+require_once("Themes/Settings.php");
 ?>
 <!--Header Theme -->
 <!-- function to check if user is logged in here-->
@@ -9,7 +9,7 @@
 <!--logged in user view-->
     <title>Tofoo Food Delivery</title>
 
-    <nav class="navbar navbar-expand-lg" style="background-color:lightcoral">
+    <nav class="navbar navbar-expand-xl" style="background-color:lightcoral">
       <div class="container-fluid">
         <a class="navbar-brand" href="#"><h2>ToFoo</h2></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,14 +18,14 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#"><h3>Account<h3></a>
+          <a class="nav-link" aria-current="page" href="#"><h1>Account<h1></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><h3>Signout<h3></a>
+          <a class="nav-link" href="#"><h1>Signout<h1></a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <h3>Cart</h3>
+            <h1>Cart</h1>
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">View</a></li>
--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
 session_start();
 require_once('Themes/Header.php');
+require_once('settings.php');
+// Simple query
+$result=$connection->query('
+	SELECT restaurant.ID,restaurant.Name AS name_count 
+	FROM restaurant
+	GROUP BY restaurant.ID');
 ?>
+<?php
+require_once('Themes/Body.php');
+while($restaurant=$result->fetch()){
+echo '<div class="row row-cols-3">
+	<div class="col">
+		<div class="card" style="width: 18rem;">
+ 			<img class="card-img-top" src="..." alt="Card image">
+  				<div class="card-body">
+    				<h5 class="card-title">Restaurant</h5>
+    				<p class="card-text">Restaurant Description</p>
+    				<a href="#" style="background-color:lightcoral" class="btn btn-outline-light">View</a>
+  				</div>
+		</div>
+	</div>
+</div>';
+}
+?>
+</div>
 
 <?php
 	require_once('Themes/Footer.php')
