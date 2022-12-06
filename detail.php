8be107b5f7e0cb6b9ba51cc1dd14b4edf0464650
Angle-M <98354046+Angle-M@users.noreply.github.com>
2022-12-06 16:34:33 -0500

    created detail page and adjusted some code

---
+++ detail.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once('Themes/Header.php');
+require_once('settings.php');
+require_once('Themes/Body.php');
+$result=$connection->query('SELECT ID FROM restaurant
+
+');
+?>
+<div class="container py-5">
+    <div class="row">
+
+    </div>
+</div>
+<?php
+require_once('Themes/Footer.php');
+?>
--- index.php
+++ index.php
@@ -4,17 +4,16 @@ require_once('Themes/Header.php');
 require_once('settings.php');
 // Simple query
 $result=$connection->query('SELECT * FROM restaurant');
-
+require_once('Themes/Body.php');
 ?>
 <div class="container py-5">
-<div class="row">
-<?php
-require_once('Themes/Body.php');
-while($restaurant=$result->fetch()){
-	?>
+	<div class="row">
+		<?php
+		while($restaurant=$result->fetch()){
+		?>
 	<div class="col-md-4">
 		<div class="card">
- 			<img class="card-img-top" src="..." alt="Card image">
+ 			<img class="card-img-top" src="#" alt="Card image">
   				<div class="card-body">
     				<h5 class="card-title"><?= $restaurant['Name'] ?></h5>
     				<p class="card-text">Location: <?=$restaurant['Street1'].', '.$restaurant['Street2'].', '.$restaurant["City"]?></p>
